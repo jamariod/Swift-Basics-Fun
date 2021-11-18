f6d16b8f331004a12ff2db1dce8f1f671a01b016
jamariod <j25ghs99@aol.com>
2021-11-18 15:01:20 -0500

    Arrays, Random Numbers, Conditionals, and Loops

--- Swift Basics Fun/main.swift
+++ Swift Basics Fun/main.swift
@@ -7,36 +7,101 @@
 
 import Foundation
 
-print("Hello, World!")
+//print("Hello, World!")
+//
+//print("Another print statement!")
+//
+//// one line comment
+//
+///*
+// multi line comment
+// */
+//
+//// MARK: - Variables
+//// every type in swift is an object, which means there are no primitives
+//var temperature: Int = 75
+//print(temperature)
+//
+//var temperature2 = 74.9
+//print(temperature2)
+//// Int, Float, Double, Bool, Character, String, Array, Set, Dictionary, etc.
+//
+//// use let to declare a constant
+//let month = "September"
+//let day = 1
+//print(month)
+//// string interpolation
+//
+//print("Today is \(month) \(day)")
+//// task 1: declare two constants, one for your first name and one for your last name
+//// print on one line "jon snow has 7 characters"
+//let firstName = "jon"
+//let lastName = "snow"
+//let numChars = firstName.count + lastName.count
+//print("\(firstName) \(lastName) has \(numChars) characters")
 
-print("Another print statement!")
+// MARK: Arrays
+// same type element
+//var food = ["eggs", "bacon", "bagels"] // [String]
+//print(food)
+//
+//print(food[0])
+//print(food.count)
+//print(food.isEmpty)
+//// Arrays can grow and shrink in size
+//food.append("coffee")
+//print(food)
+//// task 2: try removing the first element
+//food.remove(at: 0)
+//print(food)
+//
+//// MARK: Random Numbers
+//// throw a 6 sided dice and save it in randNum
+//var randNum = arc4random_uniform(6) + 1 // Add 1 to arc4random_uniform to include the upperbound number
+//print(randNum)
+//var randNumAsInt = Int(randNum) // Type cast randNum from UInt32 to Int
+//
+//// MARK - Conditionals
+//
+//var x = 100
+//if x > 90 {
+//    print("x is greater than 90")
+//}
+//
+//// MARK: - Loops
+//// for in loop
+//for i in 0..<10 {
+//    print(i, terminator: ", ") // terminator is used to print all numbers on the same line instead of creating a new line.
+//}
+//// close range 0...10 inclusive of the end value
+//print()
 
-// one line comment
+// 2 other loops in swift
+//1. while loop
+//2. repeat-while
 
-/*
- multi line comment
- */
+// task: solve the same problem using each of the 3 loops
+// print the first 20 even numbers separated by spaces
+// 2 4 6 ... 40
 
-// MARK: - Variables
-// every type in swift is an object, which means there are no primitives
-var temperature: Int = 75
-print(temperature)
+for i in 1...40 {
+    if i % 2 == 0 {
+        print(i, terminator: " ")
+    }
+}
+print()
 
-var temperature2 = 74.9
-print(temperature2)
-// Int, Float, Double, Bool, Character, String, Array, Set, Dictionary, etc.
 
-// use let to declare a constant
-let month = "September"
-let day = 1
-print(month)
-// string interpolation
+var x = 2
+while x <= 40 {
+  print(x, terminator: " ")
+  x += 2
+}
+print()
 
-print("Today is \(month) \(day)")
-// task 1: declare two constants, one for your first name and one for your last name
-// print on one line "jon snow has 7 characters"
-let firstName = "jon"
-let lastName = "snow"
-let numChars = firstName.count + lastName.count
-print("\(firstName) \(lastName) has \(numChars) characters")
-
+var j = 2
+repeat {
+    print(j, terminator: " ")
+    j += 2
+} while j <= 40
+print()
